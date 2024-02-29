6edd8f8bee84e4f26d1c03ef7b8f0ce8cb6768c9
chspDEV <chspcontato@gmail.com>
2024-02-29 08:42:24 -0300

    teste multiplayer04

--- TCP3/Assets/Resources/GPTMOVIMENTO.cs
+++ TCP3/Assets/Resources/GPTMOVIMENTO.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GPTMOVIMENTO : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidadeMovimento = 5f; // Velocidade de movimento do objeto
+    public float sensibilidadeMouse = 2f; // Sensibilidade do mouse para rotação
+
+    private float rotationX = 0f;
+
     void Start()
     {
-        
+        // Esconder e travar o cursor do mouse
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Movimento do jogador
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * moveHorizontal + transform.forward * moveVertical;
+        transform.Translate(moveDirection * velocidadeMovimento * Time.deltaTime, Space.World);
+
+        // Rotação do jogador com base no mouse
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidadeMouse;
+        rotationX -= Input.GetAxis("Mouse Y") * sensibilidadeMouse;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
     }
 }
