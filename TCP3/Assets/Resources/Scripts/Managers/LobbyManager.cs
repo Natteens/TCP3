d3817969faa3c8d8bb10b8b4dfb556e2796124a1
Natte <107698186+Natteens@users.noreply.github.com>
2024-03-07 21:36:11 -0300

    LobbyManager P1

--- TCP3/Assets/Resources/Scripts/Managers/LobbyManager.cs
+++ TCP3/Assets/Resources/Scripts/Managers/LobbyManager.cs
@@ -10,7 +10,10 @@ using TMPro;
 
 public class LobbyManager : MonoBehaviour
 {
-    public TMP_InputField playerNameInput, lobbyCodeInput; 
+    public TMP_InputField playerNameInput, lobbyCodeInput;
+    public GameObject introLobby, lobbyPanel;
+    public TMP_Text lobbyCodeText;
+    public TMP_Text[] playerNameText;
     Lobby hostLobby, joinnedLobby;
 
     async void Start()
@@ -20,6 +23,10 @@ public class LobbyManager : MonoBehaviour
 
     async Task Authenticate()
     {
+        if(AuthenticationService.Instance.IsSignedIn)
+         return; 
+
+        AuthenticationService.Instance.ClearSessionToken();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
         Debug.Log("Usuário logado como " + AuthenticationService.Instance.PlayerId);
     }
@@ -27,14 +34,76 @@ public class LobbyManager : MonoBehaviour
     async public void CreateLobby()
     {
         await Authenticate();
-        hostLobby = await Lobbies.Instance.CreateLobbyAsync("lobby", 20);
+
+        CreateLobbyOptions options = new CreateLobbyOptions
+        {
+            Player = GetPlayer()
+        };
+
+        hostLobby = await Lobbies.Instance.CreateLobbyAsync("lobby", 20, options);
+        joinnedLobby = hostLobby;
         Debug.Log("Criou o lobby" + hostLobby.LobbyCode);
+        InvokeRepeating("SendLobbyHeartBeat", 10, 10);
+        UpdateLobby();
+        ShowPlayer();
+
+        lobbyCodeText.text = joinnedLobby.LobbyCode;
+        introLobby.SetActive(false);
+        lobbyPanel.SetActive(true);
+    }
+
+    Player GetPlayer()
+    {
+        Player player = new Player
+        {
+            Data = new Dictionary<string, PlayerDataObject>
+            {
+                { "name", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, playerNameInput.text) }
+            }
+        };
+        return player;
     }
 
     async public void JoinLobbyByCode()
     {
         await Authenticate();
-        joinnedLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCodeInput.text);
+
+        JoinLobbyByCodeOptions options = new JoinLobbyByCodeOptions
+        {
+            Player = GetPlayer()
+        };
+
+        joinnedLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCodeInput.text, options);
         Debug.Log("Entrou no lobby" + joinnedLobby.LobbyCode);
+        UpdateLobby();
+        ShowPlayer();
+
+        lobbyCodeText.text = joinnedLobby.LobbyCode;
+        introLobby.SetActive(false);
+        lobbyPanel.SetActive(true);
+    }
+
+    async void SendLobbyHeartBeat()
+    {
+        if(hostLobby == null)
+            return;
+        await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+        Debug.Log("Atualizou o Lobby");
+        ShowPlayer();
+    }
+
+    void ShowPlayer()
+    {
+        for(int i = 0; i<joinnedLobby.Players.Count; i++)
+        {
+            playerNameText[i].text = joinnedLobby.Players[i].Data["name"].Value;
+        }
+    }
+
+    async void UpdateLobby()
+    {
+        if(joinnedLobby == null) return;
+
+        joinnedLobby = await LobbyService.Instance.GetLobbyAsync(joinnedLobby.Id);
     }
 }
