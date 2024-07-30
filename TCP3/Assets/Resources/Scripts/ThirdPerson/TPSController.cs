f200cc4cd9b6141f8bebe6ddf560514cb3f1d482
Natteens <ngamerandroid@gmail.com>
2024-07-30 13:39:12 -0300

    fixs

--- TCP3/Assets/Resources/Scripts/ThirdPerson/TPSController.cs
+++ TCP3/Assets/Resources/Scripts/ThirdPerson/TPSController.cs
@@ -18,13 +18,49 @@ public class TPSController : MonoBehaviour
 
     private StarterAssetsInputs starterAssetsInputs;
     private ThirdPersonController thirdPersonController;
-
+    [SerializeField] private float speedRotate;
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
     }
     private void Update()
+    {
+        Vector3 mouseWorldPosition = Raycast();
+        Aim(mouseWorldPosition);
+        Shoot(mouseWorldPosition);
+    }
+
+    private void Shoot(Vector3 mouseWorldPosition)
+    {
+        if (starterAssetsInputs.shoot)
+        {
+            Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
+            Rotation(LookDir(mouseWorldPosition), speedRotate);
+            Instantiate(pfBulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            starterAssetsInputs.shoot = false;
+        }
+    }
+
+    private void Aim(Vector3 mouseWorldPosition)
+    {
+        if (starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+            Vector3 aimDirection = LookDir(mouseWorldPosition);
+            Rotation(aimDirection, 20f);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetRotateOnMove(true);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+        }
+    }
+
+    private Vector3 Raycast()
     {
         Vector3 mouseWorldPosition = Vector3.zero;
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
@@ -36,33 +72,19 @@ public class TPSController : MonoBehaviour
             debugTransform.position = raycasthit.point;
         }
 
-        if (starterAssetsInputs.aim)
-        {
-            aimVirtualCamera.gameObject.SetActive(true);
-            thirdPersonController.SetSensitivity(aimSensitivity);
-            thirdPersonController.SetRotateOnMove(false);
-
-            Vector3 worldAimTarget = mouseWorldPosition;
-            worldAimTarget.y = transform.position.y;
-            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
-
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
-        }
-        else
-        {
-            aimVirtualCamera.gameObject.SetActive(false);
-            thirdPersonController.SetRotateOnMove(true);
-            thirdPersonController.SetSensitivity(normalSensitivity);
-        }
-
-        if (starterAssetsInputs.shoot)
-        { 
-            Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
-            Instantiate(pfBulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
-            starterAssetsInputs.shoot = false;
-        }
-
+        return mouseWorldPosition;
     }
 
+    private Vector3 LookDir(Vector3 mouseWorldPosition)
+    {
+        Vector3 worldAimTarget = mouseWorldPosition;
+        worldAimTarget.y = transform.position.y;
+        Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+        return aimDirection;
+    }
 
+    private void Rotation(Vector3 aimDirection, float speed)
+    {
+        transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * speed);
+    }
 }
