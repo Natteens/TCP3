0c285868f09f5c981220dd69b49a440147ef93c6
Natteens <ngamerandroid@gmail.com>
2024-08-10 21:17:22 -0300

    ajustes

--- TCP3/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ TCP3/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -262,8 +262,7 @@ namespace StarterAssets
                 _speed = targetSpeed;
             }
 
-            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
-            if (_animationBlend < 0.01f) _animationBlend = 0f;
+            _animationBlend = (targetSpeed == 0f) ? 0f : (targetSpeed == MoveSpeed) ? 1f : 2f;
 
             // normalise input direction
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
