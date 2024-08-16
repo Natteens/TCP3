055c18253e3934e9e7b49f14a1877eb6a3447cb0
Natteens <ngamerandroid@gmail.com>
2024-08-16 12:47:06 -0300

    Update PlayerName.cs

--- TCP3/Assets/Resources/Scripts/Networking/PlayerName.cs
+++ TCP3/Assets/Resources/Scripts/Networking/PlayerName.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using TMPro;
+using Unity.Collections; // Necessário para FixedString
 
 public class PlayerName : NetworkBehaviour
 {
-    // Variável de rede para armazenar o nome do jogador
-    private NetworkVariable<string> playersName = new NetworkVariable<string>();
+    // Usando FixedString64Bytes para serializar o nome do jogador
+    private NetworkVariable<FixedString64Bytes> playersName = new NetworkVariable<FixedString64Bytes>();
 
     private bool overlaySet = false;
 
@@ -18,7 +19,7 @@ public class PlayerName : NetworkBehaviour
             // Defina o nome do jogador com base no LobbyManager
             string name = LobbyManager.Instance.GetName() != null ? LobbyManager.Instance.GetName() : $"Player {OwnerClientId}";
 
-            // Atualize o valor da variável de rede
+            // Atualize o valor da variável de rede com o nome do jogador
             playersName.Value = name;
         }
 
@@ -29,12 +30,12 @@ public class PlayerName : NetworkBehaviour
     public void SetOverlay()
     {
         var localPlayerOverlay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
-        localPlayerOverlay.text = playersName.Value;
+        localPlayerOverlay.text = playersName.Value.ToString();
     }
 
     void Update()
     {
-        if (!overlaySet && !string.IsNullOrEmpty(playersName.Value))
+        if (!overlaySet && !string.IsNullOrEmpty(playersName.Value.ToString()))
         {
             SetOverlay();
             overlaySet = true;
